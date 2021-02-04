949d6711f091e1e06bc2b70bde622d1eee9fe814
unicorn1206 <Stephanie_eckel@126.com>
2021-02-04 14:37:42 +0800

    1.链表：使用虚拟头节点，在头部添加元素可以使用通用的add方法。

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -25,11 +25,11 @@ public class LinkedList<E> {
         }
     }
 
-    private Node head;
+    private Node dummyhead;
     private int size;
 
     public LinkedList(){
-        head = null;
+        dummyhead = new Node(null,null);
         size = 0;
     }
 
@@ -41,31 +41,23 @@ public class LinkedList<E> {
         return size == 0;
     }
 
-    //在链表头添加新的元素E
-    public void addFirst(E e){
-//        Node node = new Node(e);
-//        node.next = head;
-//        head = node;
-        head = new Node(e,head);
-        size++;
-    }
-
     //在链表的index位置添加新元素
     public void add(E e,int index){
         if(index < 0 || index > size){
             throw new IllegalArgumentException("Add failed. Index is illegal.");
         }
-        if(index == 0){
-            addFirst(e);
-        }else{
-            Node prev = head;
-            for (int i = 0; i < index - 1; i++) {
-                prev = prev.next;
-            }
-            Node node = new Node(e,prev.next);
-            prev.next = node;
-            size++;
+
+        Node prev = dummyhead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
         }
+        Node node = new Node(e,prev.next);
+        prev.next = node;
+        size++;
+    }
+
+    public void addFirst(E e) {
+        add(e,0);
     }
 
     //在链表尾部添加元素
